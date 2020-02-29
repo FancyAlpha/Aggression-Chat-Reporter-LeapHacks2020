091806d4907d921705b2ac6c8b3b24c18b50d62b
JibranK12345 <jibranraahim123@gmail.com>
2020-02-29 13:58:20 -0600

    Rest API

---
+++ api/server.js
@@ -0,0 +1,18 @@
+require('dotenv').config();
+
+const express = require('express');
+const mongoose = require('mongoose');
+const foodRouter = require('./routes/commentRoutes.js');
+const username = process.env.DATABASE_USERNAME;
+const password = process.env.DATABASE_PASSWORD;
+
+const app = express();
+app.use(express.json()); // Make sure it comes back as json
+
+mongoose.connect('mongodb+srv://twoligma:tangerine8@cluster0-agcvh.mongodb.net/test?retryWrites=true&w=majority', {
+  useNewUrlParser: true
+});
+
+app.use(foodRouter);
+
+app.listen(3000, () => { console.log('Server is running...') });
